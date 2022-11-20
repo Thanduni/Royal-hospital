da0c9aae6af680b2c47238ddce00b72d83d38237
Nareash <nareash20010150@gmail.com>
2022-11-20 07:09:27 +0530

    Resolved login problem with hash password

--- RoyalHospital/loginProcess.php
+++ RoyalHospital/loginProcess.php
@@ -2,50 +2,50 @@
 require_once("DBconnect.php");
 session_start();
 
+//$_SESSION['mailaddress'] = "nareash";
+//header("location:adminDash.php");
+//exit(0);
+
 if (isset($_POST['login'])) {
     if (empty($_POST["email"]) || empty($_POST["password"])) {
-
         header("location:login.php?Empty=Please fill in the blanks");
     } else {
-        $query = "select * from user where email='" . $_POST["email"] . "' and password='" . $_POST['password'] . "'";
+        $query = $con -> prepare("select name, email, password, user_role, profile_image from user where email=?;");
+        $query -> bind_param("s", $_POST["email"]);
+        $query->execute();
+        $query->store_result();
+        $query ->bind_result($name, $email,$password, $userRole, $profilePic);
 
-        $result = mysqli_query($con, $query);
+        if ($query -> num_rows == 1) {
+            $pass = $_POST['password'];
 
-        if (mysqli_fetch_assoc($result)) {
-            $_SESSION['mailaddress'] = $_POST["email"];
+            $query -> fetch();
 
-            $query_2 =  "select name from user where email='" . $_POST["email"] . "' and password='" . $_POST['password'] . "'";
+            if (password_verify($pass, $password)) {
 
-            $result_2 = mysqli_query($con, $query_2);
-
-            $query_3 = "select user_role from user where email='" . $_POST["email"] . "' and password='" . $_POST['password'] . "'";
-
-            $result_3 = mysqli_query($con, $query_3);
-
-            $row_3 = $result_3->fetch_assoc();
-
-            $row_1 = $result_2->fetch_assoc();
-
-            $_SESSION['name'] = $row_1['name'];
-
-            if($row_3['user_role'] == "Admin")
-                header("location:adminDash.php");
-            else if($row_3['user_role'] == "Doctor")
-                header("location:doctorDash.php");
-            else if($row_3['user_role'] == "Nurse")
-                header("location:nurseDash.php");
-            else if($row_3['user_role'] == "Receptionist")
-                header("location:receptionistDash.php");
-            else if($row_3['user_role'] == "Patient")
-                header("location:patientDash.php");
-            else if($row_3['user_role'] == "Storekeeper")
-                header("location:storekeeperDash.php");
+                $_SESSION['name'] = $name;
+                $_SESSION['profilePic'] = $profilePic;
+                if ($userRole == "Admin")
+                    header("location:adminDash.php");
+                else if ($userRole == "Doctor")
+                    header("location:doctorDash.php");
+                else if ($userRole == "Nurse")
+                    header("location:nurseDash.php");
+                else if ($userRole == "Receptionist")
+                    header("location:receptionistDash.php");
+                else if ($userRole == "Patient")
+                    header("location:adminDash.php");
+                else if ($userRole == "Storekeeper")
+                    header("location:storekeeperDash.php");
+                $_SESSION['mailaddress'] = $_POST["email"];
+            } else {
+                header("location:login.php?Invalid=Incorrect login credentials i.e. email or password!");
+            }
         } else {
-
             header("location:login.php?Invalid=Incorrect login credentials i.e. email or password!");
         }
     }
 } else {
-
     die("not working");
 }
+
