8fea26d6452da9ea533b9cac1979d7c4dfc8f713
anjana-nanayakkara <anjanananayakkara1999@gmail.com>
2023-01-05 11:29:42 +0530

    setup and connect storekeeper envirnment

--- Storekeeper/storekeeperDash.php
+++ Storekeeper/storekeeperDash.php
@@ -1,9 +1,9 @@
-<!-- <?php
+ <?php
 session_start();
 //die( $_SESSION['profilePic']);
 require_once("../conf/config.php");
-if (isset($_SESSION['mailaddress'])) {
-?> -->
+if (isset($_SESSION['mailaddress']) && isset($_SESSION['userRole']) && $_SESSION['userRole']=="Storekeeper") {
+?> 
 
 <!doctype html>
 <html lang="en">
@@ -12,7 +12,7 @@ if (isset($_SESSION['mailaddress'])) {
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <link rel="stylesheet" href="<?php echo BASEURL . '/css/style.css' ?>">
-    <link rel="stylesheet" href="<?php echo BASEURL . '/css/adminDash.css' ?>">
+    <link rel="stylesheet" href="<?php echo BASEURL . '/css/adminDashCopy.css' ?>">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <style>
         .next {
@@ -20,19 +20,19 @@ if (isset($_SESSION['mailaddress'])) {
             height: auto;
         }
     </style>
-    <title>Admin Users</title>
+    <title>Storekeeper Dashboard</title>
 </head>
 <body>
 <div class="user">
-    <?php include(BASEURL . '/Components/AdminSidebar.php?profilePic=' . $_SESSION['profilePic'] . "&name=" . $_SESSION['name']); ?>
+    <?php include(BASEURL . '/Components/storekeeperSidebar.php?profilePic=' . $_SESSION['profilePic'] . "&name=" . $_SESSION['name']); ?>
     <div class="userContents" id="center">
         <div class="title">
             <img src="<?php echo BASEURL . '/images/logo5.png' ?>" alt="logo">
             Royal Hospital Management System
         </div>
         <ul>
-            <li class="userType"><img src=<?php echo BASEURL . '/images/userInPage.svg' ?> alt="admin">
-                Admin
+            <li class="userType"><img src=<?php echo BASEURL . '/images/userInPage.svg' ?> alt="Storekeeper">
+                Storekeeper
             </li>
             <li class="logout"><a href="<?php echo BASEURL . '/Homepage/Logout?url=' . $_SERVER['REQUEST_URI'] ?>">Logout
                     <img
@@ -40,30 +40,9 @@ if (isset($_SESSION['mailaddress'])) {
             </li>
         </ul>
         <div class="arrow">
-            <img src=<?php echo BASEURL . '/images/arrow-right-circle.svg' ?> alt="arrow">User
+            <img src=<?php echo BASEURL . '/images/arrow-right-circle.svg' ?> alt="arrow">Dashboard
         </div>
-        <div class="actorCards">
-            <ul>
-                <a href="<?php echo BASEURL . '/Admin/adminDash.php' ?>">
-                    <li>Dashboard</li>
-                </a>
-                <a href="<?php echo BASEURL . '/Admin/adminUsersPage.php' ?>">
-                    <li>User</li>
-                </a>
-                <a href="">
-                    <li>Doctor</li>
-                </a>
-                <a href="">
-                    <li>Nurse</li>
-                </a>
-                <a href="">
-                    <li>Receptionist</li>
-                </a>
-                <a href="">
-                    <li>Noticeboard</li>
-                </a>
-            </ul>
-        </div>
+        
         <?php include(BASEURL . '/Components/Footer.php'); ?>
     </div>
 </div>
