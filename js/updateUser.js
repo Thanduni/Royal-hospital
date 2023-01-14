2e1de72fe0d3cca5ad992120994493a17a52c16f
Mr.Nareash <nareash20010150@gmail.com>
2023-01-14 08:39:06 +0530

    Commit on 14/01/2023

--- js/updateUser.js
+++ js/updateUser.js
@@ -6,7 +6,6 @@ function displayUserUpdateForm(nic) {
     form.action = '/royalhospital/Admin/updateUser.php?id=' + nic;
     document.getElementById("nicRow").classList.add("hide");
     document.getElementById("passRow").classList.add("hide");
-    document.getElementById("userRoleRow").classList.add("hide");
 
 
     let tableCon = document.getElementsByClassName(nic + "_tableCon");
@@ -18,6 +17,7 @@ function displayUserUpdateForm(nic) {
     let email = tableCon[2].textContent;
     let contactNum = tableCon[3].textContent;
     let gender = tableCon[4].textContent;
+    let userRole = tableCon[5].textContent;
 
     // alert(tableCon[0].textContent);
 
@@ -36,13 +36,33 @@ function displayUserUpdateForm(nic) {
     else
         F_gender.setAttribute("checked", "checked");
 
+    let IN_userRole = document.getElementById("IN_userRole");
+
+    IN_name.value = name;
+    IN_address.value = address;
+    IN_email.value = email;
+    IN_contnum.value = contactNum;
+    if(userRole === "Patient")
+        IN_userRole.selectedIndex = 0;
+    else if(userRole === "Doctor")
+        IN_userRole.selectedIndex = 1;
+    else if(userRole === "Receptionist")
+        IN_userRole.selectedIndex = 2;
+    else if(userRole === "Storekeeper")
+        IN_userRole.selectedIndex = 3;
+    else if(userRole === "Nurse")
+        IN_userRole.selectedIndex = 4;
     form.scrollIntoView();
+
 
     let close = document.getElementById('cancel');
     close.addEventListener('click', function() {
         userForm.classList.remove("active");
+        // document.getElementById("passToJS").innerHTML = "";
     }, false);
 }
+
+// update.addEventListener("click", displayUserForm, false);
 
 function displayDoctorUpdateForm(nic) {
     let form = document.getElementById("addForm");
@@ -54,6 +74,9 @@ function displayDoctorUpdateForm(nic) {
     document.getElementById("titleOperation").innerHTML = "Update User | NIC : " + nic;
 
     let department = tableCon[0].textContent;
+
+    // alert(tableCon[0].textContent);
+
 
     let userForm = document.getElementById('userForm');
     userForm.classList.add("active");
@@ -67,5 +90,6 @@ function displayDoctorUpdateForm(nic) {
     let close = document.getElementById('cancel');
     close.addEventListener('click', function() {
         userForm.classList.remove("active");
+        // document.getElementById("passToJS").innerHTML = "";
     }, false);
 }
