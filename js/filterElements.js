6c7f2f3893bfc9ebb170feefb1f64f1699ad3e97
Mr.Nareash <nareash20010150@gmail.com>
2023-01-18 19:55:50 +0530

    Commit on 18/01/2023

---
+++ js/filterElements.js
@@ -0,0 +1,37 @@
+function filterByName() {
+    var input, filter, table, row, cell, i, txtValue;
+    input = document.getElementById("myInputName");
+    filter = input.value.toUpperCase();
+    table = document.getElementsByClassName("table")[0];
+    row = table.getElementsByClassName("row");
+    for (i = 1; i < row.length; i++) {
+        cell = row[i].getElementsByClassName("cell")[2];
+        if (cell) {
+            txtValue = cell.textContent || cell.innerText;
+            if (txtValue.toUpperCase().indexOf(filter) > -1) {
+                row[i].style.display = "";
+            } else {
+                row[i].style.display = "none";
+            }
+        }
+    }
+}
+
+function filterByRole() {
+    var input, filter, table, row, cell, i, txtValue;
+    input = document.getElementById("myInputRole");
+    filter = input.value.toUpperCase();
+    table = document.getElementsByClassName("table")[0];
+    row = table.getElementsByClassName("row");
+    for (i = 1; i < row.length; i++) {
+        cell = row[i].getElementsByClassName("cell")[4];
+        if (cell) {
+            txtValue = cell.textContent || cell.innerText;
+            if (txtValue.toUpperCase().indexOf(filter) > -1) {
+                row[i].style.display = "";
+            } else {
+                row[i].style.display = "none";
+            }
+        }
+    }
+}
