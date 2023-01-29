bcd5785b92c1f780c849d59ede375eded7216773
Thanduni <76160782+Thanduni@users.noreply.github.com>
2023-01-29 16:18:20 +0530

    Changed user interfece

--- Nurse/display.php
+++ Nurse/display.php
@@ -1,21 +1,18 @@
+<?php
+session_start();
+//die( $_SESSION['profilePic']);
+require_once("../conf/config.php");
+if (isset($_SESSION['mailaddress']) && $_SESSION['userRole'] == 'Nurse') {
+    ?>
 
 <?php
-$page = 'display';
-include '../Components/nurseSidebar.php';
-include '../Components/nursetopbar.php';
-require_once("../conf/config.php")
-?>
-
-<?php
-// include 'connect.php';
 if(isset($_POST['submit'])){
-    $patientID =  $_POST['patientID'];
+    // $patientID =  $_POST['patientID'];
     $RoomNo = $_POST['roomNo'];
     $admit_date = $_POST['admit_date'];
     $admit_time = $_POST['admit_time'];
 
     $sql="INSERT INTO inpatient(admit_time,admit_date,roomNo) values('$admit_time','$admit_date','$RoomNo');";
-
     $result=mysqli_query($con,$sql);
 
     if($result){
@@ -27,135 +24,134 @@ if(isset($_POST['submit'])){
 }
 ?>
 
-<style>
-    <?php include '../css/nurseStyle.css';
-    ?>
-</style>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
+    <link rel="stylesheet" href="<?php echo BASEURL . '/css/style.css' ?>">
+    <link rel="stylesheet" href="<?php echo BASEURL . '/css/nurseStyle.css' ?>">
+    <style>
+        .next {
+            position: initial;
+            height: auto;
+        }
+    </style>
+    <title>Patient</title>
  
 </head>
 <body>
+    <div class="user">
+        <?php include(BASEURL . '/Components/nurseSidebar.php?profilePic=' . $_SESSION['profilePic'] . "&name=" . $_SESSION['name']); ?>
+        <div class="userContents" id="center">
+            <div class="title">
+                <img src="<?php echo BASEURL . '/images/logo5.png' ?>" alt="logo">
+                Royal Hospital Management System
+            </div>
+            <ul>
+                <li class="userType"><img src=<?php echo BASEURL . '/images/userInPage.svg' ?> alt="admin">
+                    Nurse
+                </li>
+                <li class="logout"><a href="<?php echo BASEURL . '/Homepage/logout.php?logout' ?>">Logout
+                        <img
+                                src=<?php echo BASEURL . '/images/logout.jpg' ?> alt="logout"></a>
+                </li>
+            </ul>
+            <div class="arrow">
+                <img src=<?php echo BASEURL . '/images/arrow-right-circle.svg' ?> alt="arrow">Dashboard
+            </div>
 
-    <div class="main-container">
-        
-        <!-- <button class="button" id="button"><a href="admissionForm.php">
-            New admission</a>
-        </button> -->
+            <div class="main-container">
 
-        <button class="button" id="admission-button">
-            New Admission
-        </button>
+                <button class="button" id="admission-button">
+                    New Admission
+                </button>
 
-        <script>
-            document.getElementById("admission-button").addEventListener("click", function(){
-                document.querySelector("#admission-popup").style.display = "flex";
-            // document.querySelector(".popup").style.display = "flex";
-        })
+                <div class="table-container">
+                    <table class="table">
 
-        </script>
+                        <thead>    
+                            <th>Name</th>
+                            <th>Room No</th>
+                            <th>admit date</th>
+                            <th>admit time</th>
+                            <th>Drug allergies</th>
+                            <th>Emergency No</th>
+                        </thead>
 
-        <div class="table-container">
-            <table class="table">
-                <thead>    
-                    <th>Name</th>
-                    <th>roomNo</th>
-                    <th>admit date</th>
-                    <th>admit time</th>
-                    <th>Drug allergies</th>
-                    <th>Emergency No</th>
-                </thead>
+                        <tbody>
 
-                <tbody>
+                            <?php
+                                $sql="select user.name,inpatient.roomNo,inpatient.admit_date,inpatient.admit_time,patient.drug_allergies,patient.emergency_contact from user join patient on user.nic=patient.nic join inpatient on inpatient.patientID=patient.patientID;";
+                                $result=mysqli_query($con,$sql);
 
-            <?php
-$sql="select user.name,inpatient.roomNo,inpatient.admit_date,inpatient.admit_time,patient.drug_allergies,patient.emergency_contact from user join patient on user.nic=patient.nic join inpatient on inpatient.patientID=patient.patientID;";
-$result=mysqli_query($con,$sql);
+                                if($result){
+                                while($row=mysqli_fetch_assoc($result)){
+                                $name =  $row['name'];
+                                $RoomNo = $row['roomNo'];
+                                $admit_date = $row['admit_date'];
+                                $admit_time = $row['admit_time'];
+                                $drug_allergies = $row['drug_allergies'];
+                                $emergency_contact = $row['emergency_contact'];
+                                echo '<tr> 
 
-if($result){
-    while($row=mysqli_fetch_assoc($result)){
-        $name =  $row['name'];
-        $RoomNo = $row['roomNo'];
-        $admit_date = $row['admit_date'];
-        $admit_time = $row['admit_time'];
-        $drug_allergies = $row['drug_allergies'];
-        $emergency_contact = $row['emergency_contact'];
-        echo '<tr> 
+                                <td>'.$name.'</td>
+                                <td>'.$RoomNo.'</td>
+                                <td>'.$admit_date.'</td>
+                                <td>'.$admit_time.'</td>
+                                <td>'.$drug_allergies.'</td>
+                                <td>'.$emergency_contact.'</td>
+                                </tr>';
+                                }
+                                }
+                            ?>    
+                        </tbody>
+                    </table>
+                </div>
+            </div>
 
-        <td>'.$name.'</td>
-        <td>'.$RoomNo.'</td>
-        <td>'.$admit_date.'</td>
-        <td>'.$admit_time.'</td>
-        <td>'.$drug_allergies.'</td>
-        <td>'.$emergency_contact.'</td>
-    </tr>';
-    }
-}
-    ?>    
-            </tbody>
-            
-        </table>
         </div>
     </div>
 
     <div class="popup" id="admission-popup">
-        <div class="popup-content">
-        <form method="post">
-            <h1>Admission Form</h1>
-            <div class="form-group">
-                <label>Name</label>
-                <input type="text" class="form-control" placeholder="Enter name" name="name" required>
-            </div>
-            <div class="form-group">
-                <label>Room No</label>
-                <input type="text" class="form-control" placeholder="Enter room No" name="roomNo" required>
-            </div>
-            <div class="form-group">
-                <label>Admit date</label>
-                <input type="date" class="form-control" placeholder="" name="admit_date" required>
-            </div>
-            <div class="form-group">
-                <label>Admit time</label>
-                <input type="time" class="form-control" placeholder="" name="admit_time" required>
-            </div>
-            <button type="submit" name ="submit">Submit</button>
-            <!-- <input type="submit" name="submit" value="Cancel" formnovalidate> -->
-        </form>
+        <div class="popup-content">  
+            <img src="../images/close.png" alt="close" class="close">
+            <form method="post">
+                <h3>Admission Form</h3>
+                <div class="form-group">
+                    <label>Name</label>
+                    <input type="text" class="form-control" placeholder="Enter name" name="name">
+                </div>
+                <div class="form-group">
+                    <label>Room No</label>
+                    <input type="text" class="form-control" placeholder="Enter room No" name="roomNo" >
+                </div>
+                <div class="form-group">
+                    <label>Admit date</label>
+                    <input type="date" class="form-control" placeholder="" name="admit_date" >
+                </div>
+                <div class="form-group">
+                    <label>Admit time</label>
+                    <input type="time" class="form-control" placeholder="" name="admit_time">
+                </div>
+                <button class="submit" type="submit" name ="submit">Submit</button>
+            </form>
         </div>
     </div>
+<script>
+   document.getElementById("admission-button").addEventListener("click", function(){
+        document.querySelector("#admission-popup").style.display = "flex";
+    })
+    document.querySelector(".close").addEventListener("click", function(){
+        document.querySelector("#admission-popup").style.display = "none";
+    })
+</script>
 
-    <!-- <div class="popup" id="report-popup">
-        <div class="popup-content">
-            <h1>Daily Report</h1>
-            <div class="form-group">
-                <label>Name</label>
-                <input type="text" class="form-control" placeholder="Enter name" name="name" required>
-            </div>
-            <div class="form-group">
-                <label>Room No</label>
-                <input type="text" class="form-control" placeholder="Enter room No" name="roomNo" required>
-            </div>
-            <div class="form-group">
-                <label>Admit date</label>
-                <input type="date" class="form-control" placeholder="" name="admit_date" required>
-            </div>
-            <div class="form-group">
-                <label>Admit time</label>
-                <input type="time" class="form-control" placeholder="" name="admit_time" required>
-            </div>
-            <div class="form-group">
-                <label>Emergency contact</label>
-                <input type="text" class="form-control" placeholder="" name="emergency_contact_num" required>
-            </div>
-            <button type="submit" name ="submit">Submit</button>
-        </div>
-    </div> -->
 </body>
 </html>
+<?php
+} else {
+    header("location: " . BASEURL . "/Homepage/login.php");
+}
+?>
