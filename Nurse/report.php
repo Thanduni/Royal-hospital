419d475d221b3ce8bf8ef13590c62c8870c92d94
Thanduni <76160782+Thanduni@users.noreply.github.com>
2023-02-06 22:56:15 +0530

    Update report.php

--- Nurse/report.php
+++ Nurse/report.php
@@ -30,7 +30,7 @@ if (isset($_SESSION['mailaddress']) && $_SESSION['userRole'] == 'Nurse') {
                 Royal Hospital Management System
             </div>
             <ul>
-                <li class="userType"><img src=<?php echo BASEURL . '/images/userInPage.svg' ?> alt="admin">
+                <li class="userType"><img src=<?php echo BASEURL . '/images/userInPage.svg' ?> alt="nurse">
                     Nurse
                 </li>
                 <li class="logout"><a href="<?php echo BASEURL . '/Homepage/logout.php?logout' ?>">Logout
@@ -39,7 +39,7 @@ if (isset($_SESSION['mailaddress']) && $_SESSION['userRole'] == 'Nurse') {
                 </li>
             </ul>
             <div class="arrow">
-                <img src=<?php echo BASEURL . '/images/arrow-right-circle.svg' ?> alt="arrow">Dashboard
+                <img src=<?php echo BASEURL . '/images/arrow-right-circle.svg' ?> alt="arrow">Report
             </div>
 
 
