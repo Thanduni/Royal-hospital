8c91021b406627b22cae1c5acfe2006a8f93ddc5
Thanduni <76160782+Thanduni@users.noreply.github.com>
2023-02-15 17:56:13 +0530

    added topbar component

---
+++ Components/topbar.php
@@ -0,0 +1,38 @@
+            <style>
+              <?php 
+                include '../css/nursetopbar.css';
+              ?>
+            </style>
+            <div class="topbar">
+              <div class="action">
+                  <div class="profile" onclick="menuToggle()";>
+                    <?php
+                      $profilePic = $_GET['profilePic'];
+                      echo
+                      "<p align='middle'>"
+                    ?>
+                      <img class='profilePic' align='middle' src=<?php echo BASEURL . '/uploads/' . $profilePic ?>
+                      alt='Upload Image'>
+                  </div>
+                  <div class="menu">
+                      <h3><?php echo $_SESSION['name']; ?><br><span><?php echo $_SESSION['userRole'] ?></span></h3>
+                      <ul class="topbarul">
+                          <li><img src="../images/user.svg" alt="profile image"><a href="#">My Profile</a></li>
+                          <li><img src="../images/edit.svg" alt="profile image"><a href="#">Edit Profile</a></li>
+                          <li><img src="../images/logout.svg" alt="profile image"><a href="<?php echo BASEURL . '/Homepage/logout.php?logout' ?>">Logout</a></li>
+                      </ul>
+                  </div>
+              </div>
+            </div>
+              
+            <script>
+                function menuToggle(){
+                    const togglemenu = document.querySelector('.menu');
+                    togglemenu.classList.toggle('active')
+                }
+             </script> 
+
+
+
+
+
