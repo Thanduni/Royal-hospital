2a2ffd96facccede97785d4c32f4a059042b773d
Mr.Nareash <nareash20010150@gmail.com>
2023-05-01 16:47:50 +0530

    commit on 01/05/2023 by Nareash

--- Homepage/loginProcess.php
+++ Homepage/loginProcess.php
@@ -6,7 +6,7 @@ if (isset($_POST['login'])) {
     if (empty($_POST["email"]) || empty($_POST["password"])) {
         header("location:". BASEURL . "/Homepage/login.php?Empty=Please fill in the blanks");
     } else {
-        $query = $con->prepare("select nic,name, email, password, user_role, profile_image, verify from user where email=?;");
+        $query = $con->prepare("select nic,name, email, password, user_role, profile_image from user where email=?;");
         $query->bind_param("s", $_POST["email"]);
         $query->execute();
         $query->store_result();
