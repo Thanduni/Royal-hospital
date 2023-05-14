6ab6d036e27a87143bf7c0177168e12b3adb453f
“anjana <74060929+anjana-nanayakkara@users.noreply.github.com>
2023-05-14 11:23:44 +0530

    solved error on update medicine function

--- Storekeeper/updateMedicine.php
+++ Storekeeper/updateMedicine.php
@@ -2,14 +2,13 @@
 require_once("../conf/config.php");
 if(isset($_POST['submit'])){
 
-    $itemId = $_POST['itemID'];
     $medicineName = $_POST['medicineName'];
     $companyName = $_POST['companyName'];
-    $unitType = $_POST['unitType'];
     $unitCost = $_POST['unitCost'];
+    $unitQuantity = $_POST['unitQuantity'];
     $itemID = $_GET['id'];
 
-    $sql="update `item` set item_name = '$medicineName',companyName = '$companyName',unitType = '$unitType',unit_price = '$unitCost' where itemID = '$itemID'";
+    $sql="update `item` set item_name = '$medicineName',companyName = '$companyName',`unit_quantity` = '$unitQuantity', unit_price = '$unitCost' where itemID = '$itemID'";
     $result=mysqli_query($con,$sql);
 }
 header("Location: ". BASEURL . "/Storekeeper/storekeeperAddMedicine.php");
